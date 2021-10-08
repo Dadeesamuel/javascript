b86d1e01c8f28f5765422b690bcdba453ceb1a76
DADEE SAMUEL <dadeesamuel@gmail.com>
2021-10-08 10:51:40 +0100

    javascript

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let firstName = prompt("first name");
+
+let lastName = prompt("last name");
+let userAge = prompt("your age");
+let password = prompt("input your password");
+let confirm1 = confirm("confirm your password");
+
+let answer = firstName+ " " + lastName + " " + userAge + " " + password + " " + confirm1 ;
+let fullName = firstName + " " + lastName ;
+let userName = firstName + 50 ;
+console.log( fullName);
+console.log( userAge);
+console.log( userName);
+console.log( fullName + " " + userAge + " " + userName + " " + confirm1 );
+const str =  fullName + " " + userAge + " " + userName + " " + confirm1;
+const str2 = str.toUpperCase() ;
+console.log(str2);
